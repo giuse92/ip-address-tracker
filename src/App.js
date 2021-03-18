77c578dee2d4a104663e7bee4cee97dad295be1a
Giuseppe <giuseppe.ilsami@gmail.com>
2021-03-18 15:45:08 +0100

    my own ip address on the initial page load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./App.css";
+
+const geoIpApiKey = process.env.REACT_APP_GEO_API_KEY;
 
 function App() {
+  /*const [error, setError] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [ipData, setIpData] = useState([]);
+
+  const getIpOnFirstLoading = async () => {
+    try {
+      const promise = await fetch(
+        `https://geo.ipify.org/api/v1?apiKey=${geoI}&ipAddress=`
+      );
+      const res = await promise.json();
+      if (res.code) throw new Error(res.messages);
+      setIpData(res);
+    } catch (e) {
+      setError(true);
+      console.log(e);
+    } finally {
+      setIsLoaded(true);
+    }
+  };
+
+  useEffect(() => {
+    getIpOnFirstLoading();
+  }, []);*/
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="search-ip">
+        <h1>IP Address Tracker</h1>
+        <form>
+          <input
+            placeholder="Search for any IP address or domain"
+            type="text"
+          />
+          <button>BUTTON</button>
+        </form>
+      </div>
+      <div className="show-tracker-data">
+        <ul>
+          <li>
+            <div>
+              <h4 className="data-key">IP Address</h4>
+              <p className="data-value">123.333.3.3{/*ipData.ip*/}</p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h4 className="data-key">Location</h4>
+              <p className="data-value">
+                Milan, Lombardia
+                {/*isLoaded
+                  ? `${ipData.location.city}, ${ipData.location.region}`
+                : "Loading..."*/}
+              </p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h4 className="data-key">Timezone</h4>
+              <p className="data-value">
+                UTC -5:00
+                {/*isLoaded ? `${ipData.location.timezone}` : "Loading..."}
+                {/*add offset value dynamically using the API*/}
+              </p>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h4 className="data-key">ISP</h4>
+              <p className="data-value">Vodafone{/*ipData.isp*/}</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      {
+        /*isLoaded ?*/ <MapContainer
+          style={{ height: "100vh" }}
+          center={[
+            51.505 /*ipData.location.lat*/,
+            -0.09 /*ipData.location.lng*/,
+          ]}
+          zoom={13}
+          scrollWheelZoom={false}
         >
-          Learn React
-        </a>
-      </header>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer> /*: (
+        "Loading map..."
+      )*/
+      }
     </div>
   );
 }
